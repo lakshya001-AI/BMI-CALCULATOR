681f9e4910e5aa6f6870a981109eef158fbf512c
lakshya <lakshya@tech.co>
2023-09-14 09:56:58 +0530

    Bmi-calculator

--- lib/main.dart
+++ lib/main.dart
@@ -25,48 +25,157 @@ class MyHomePage extends StatefulWidget {
 }
 class _MyHomePageState extends State<MyHomePage> {
   double height = 160; // Initial height in centimeters
+  double weight = 60; // Initial weight in kilograms
+  double bmi = 0;
+  String result = "";
+
+  void calculateBMI(){
+    setState(() {
+      bmi = weight / ((height/100)*(height/100));
+      if (bmi < 18.5) {
+        result = "Underweight";
+      } else if (bmi >= 18.5 && bmi < 24.9) {
+        result = "Normal";
+      } else {
+        result = "Overweight";
+      }
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('BMI Calculator'),
+        title: const Text('BMI Calculator'),
         backgroundColor: Colors.black,
       ),
-      body:Center(
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:  [
-            Container(height: 20),
-            const Text('Height(cm)', style: TextStyle(fontSize: 27,
-                color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius:BorderRadius.all(Radius.circular(12)),
-                  borderSide: BorderSide(color: Colors.white)
+      body:  SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 20,
+              ),
+              const Text('Height(cm)', style: TextStyle(fontSize: 27,
+                  color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 25),
+              SizedBox(
+                height: 100,
+                width: 320,
+                child: TextField(
+                  style: const TextStyle(color: Colors.white),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius:BorderRadius.all(Radius.circular(15)),
+                        borderSide: BorderSide(color: Colors.white)
+                    ),
+                    filled: true,
+                    fillColor: Colors.black,
+                    hintText: "Enter Your Height",
+                    hintStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(17))
+                    ),
+                  ),
+                  onChanged: (value) => setState(() {
+                    height = double.parse(value);
+                  }),
                 ),
-                filled: true,
-                fillColor: Colors.black,
-                hintText: "Enter Height",
-                hintStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 20), // Remove the border
-                  borderRadius: BorderRadius.circular(10.0),
+              ),
+              const Text('Weight(kg)', style: TextStyle(fontSize: 27,
+                  color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 25),
+              SizedBox(
+                height: 100,
+                width: 320,
+                child: TextField(
+                  style: const TextStyle(color: Colors.white),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius:BorderRadius.all(Radius.circular(15)),
+                        borderSide: BorderSide(color: Colors.white)
+                    ),
+                    filled: true,
+                    fillColor: Colors.black,
+                    hintText: "Enter Your Weight",
+                    hintStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(17))
+                    ),
+                  ),
+                  onChanged: (value) => setState(() {
+                    weight = double.parse(value);
+                  }),
+                ),
+              ),
+              const SizedBox(height: 3),
+              SizedBox(
+                width: 180,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {
+                    calculateBMI();
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(17)))
+                  ),
+                  child: const Text(
+                    "Calculate BMI",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                width: 250,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+                  border: Border.all(
+                    color: Colors.green, // Border color
+                    width: 2, // Border width
+                  ),
+                ),
+                child:  Center(
+                  child: Text(
+                    'Your BMI Is : ${bmi.toStringAsFixed(2)}',
+                    style: const TextStyle(fontSize: 21, color: Colors.white,  fontWeight: FontWeight.bold),
+                  ),
+                )
+              ),
+              const SizedBox(height: 26),
+              Container(
+                  width: 250,
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+                    border: Border.all(
+                      color: Colors.green, // Border color
+                      width: 2, // Border width
+                    ),
+                  ),
+                child:  Center(
+                  child: Text(
+                    "Result's : $result", style: const TextStyle(color: Colors.white, fontSize: 21,  fontWeight: FontWeight.bold),
+                  ),
                 ),
 
-              )),
+              ),
+            ],
+          ),
+        )
+      ),
+
+      );
 
 
-
-          ],
-        ) ,
-      )
-
-    );
   }
 }
 
